bb8a54e7bc3a81fd330ddef65947e8dd366abe45
Anthony Leon <acidknights89@gmail.com>
2020-10-14 23:22:55 +0000

    Automatically backed up by Learn

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -3,8 +3,9 @@ require 'net/http'
 require 'open-uri'
 require 'json'
 
-URL = https://learn-co-curriculum.github.io/json-site-example/endpoints/people.json
 class GetRequester
+  
+  URL = https://learn-co-curriculum.github.io/json-site-example/endpoints/people.json
   
   def initialize(url)
     
