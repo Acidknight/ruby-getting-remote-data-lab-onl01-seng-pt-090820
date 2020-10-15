e6aaebaa9ea01fe7c5d1f9779b8fab314a9f7f58
Anthony Leon <acidknights89@gmail.com>
2020-10-15 00:43:01 +0000

    Automatically backed up by Learn

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -17,12 +17,10 @@ class GetRequester
     response.body
   end
   
-  
-  
   def parse_json
   people = JSON.parse(self.get_response_body)
   people.collect do |people|
-    people["name", "occupation"]  
+    people["people"]  
   end
 end
 
