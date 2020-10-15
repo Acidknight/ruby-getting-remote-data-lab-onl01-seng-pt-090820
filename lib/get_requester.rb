1f20d645f956b0e17bcb7fe5276cc322d50e3e4d
Anthony Leon <acidknights89@gmail.com>
2020-10-15 00:14:14 +0000

    Automatically backed up by Learn

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -17,4 +17,11 @@ class GetRequester
     response.body
   end
   
+  def parse_json
+  programs = JSON.parse(self.get_programs)
+  programs.collect do |program|
+    program["agency"]  
+  end
+end
+  
 end
