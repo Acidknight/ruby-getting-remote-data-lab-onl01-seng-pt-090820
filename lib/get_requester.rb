d18af443295f1ba0f295afe21b9cd0299ed01355
Anthony Leon <acidknights89@gmail.com>
2020-10-15 00:12:22 +0000

    Automatically backed up by Learn

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -8,7 +8,7 @@ class GetRequester
   URL = "https://learn-co-curriculum.github.io/json-site-example/endpoints/people.json"
   
   def initialize(url)
-    uri = url.parse(URL)
+    uri = URI.parse(URL)
     response = Net::HTTP.get_response(uri)
     response.body
   end
